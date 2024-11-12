680466b2e49533f145bcea5554863bb85c81c027
abc <110215279+abc-is-here@users.noreply.github.com>
2024-11-12 07:39:37 +0530

    Timeline done!

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ setInterval(() => {
     updatedTitle = titles[Math.floor(Math.random() * titles.length)];
     document.title = "Abhay Gupta | " + updatedTitle;
 }, 2000);
+
+(function() {
+    "use strict";
+    let items = document.querySelectorAll(".timeline li");
+
+    function isElementInViewport(el) {
+        let rect = el.getBoundingClientRect();
+        return (
+            rect.top < window.innerHeight && rect.bottom >= 0
+        );
+    }
+
+    function callbackFunc() {
+        items.forEach(item => {
+            if (isElementInViewport(item)) {
+                item.classList.add("in-view");
+            }
+        });
+    }
+
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    document.addEventListener("scroll", callbackFunc);
+})();
