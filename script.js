dd5905150b9edb871404e07efd5e1131cb13a70c
abc <110215279+abc-is-here@users.noreply.github.com>
2024-11-13 13:07:21 +0530

    mouse trail

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 let titles = ["Coder", "Programmer", "Artist", "Designer", "Freelancer", "Graphics Designer", "Web Developer", "UI/UX Designer", "Frontend Developer", "Backend Developer", "Full Stack Developer", "Software Developer", "App Developer", "Game Developer", "Web Designer"];
 let updatedTitle = titles[Math.floor(Math.random() * titles.length)];
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll('.circle');
+
+const colors = ['#fff', '#d1d1d1', '#a8a8a8', '#a8a8a8'];
+
+circles.forEach(function (circle, index) {
+    circle.x = 0;
+    circle.y = 0;
+    circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener('mousemove', function (e) {
+    coords.x = e.clientX;
+    coords.y = e.clientY;
+});
+
+function animateCircles() {
+    let x = coords.x;
+    let y = coords.y;
+
+    circles.forEach(function (circle, index) {
+        circle.x += (x - circle.x) * 0.1;
+        circle.y += (y - circle.y) * 0.1;
+
+        circle.style.left = circle.x - 2 + 'px';
+        circle.style.top = circle.y - 2 + 'px';
+        circle.style.scale = (circles.length - index) / circles.length;
+
+        x = circle.x;
+        y = circle.y;
+    });
+
+    requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
 
 setInterval(() => {
     updatedTitle = titles[Math.floor(Math.random() * titles.length)];
